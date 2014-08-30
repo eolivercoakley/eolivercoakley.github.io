be23fe39a7dd2b8286c9a7ed255a2009438de1ca
Emmett Coakley <eolivercoakley@gmail.com>
2014-08-30 16:04:28 -0700

    Location changes.

--- js/app.js
+++ js/app.js
@@ -7,19 +7,19 @@ stackExchangeApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
 			when('/login', {
-				templateUrl: '../partials/loginPage.html',
+				templateUrl: 'partials/loginPage.html',
 				controller: 'LoginController'
 			}).
 			when('/question', {
-				templateUrl: '../partials/questionPage.html',
+				templateUrl: 'partials/questionPage.html',
 				controller: 'QuestionController'
 			}).
 			when('/search', {
-				templateUrl: '../partials/searchPage.html',
+				templateUrl: 'partials/searchPage.html',
 				controller: 'SearchController'
 			}).
 			when('/home', {
-				templateUrl: '../partials/homePage.html',
+				templateUrl: 'partials/homePage.html',
 				controller: 'HomeController'
 			}).
 			otherwise({
