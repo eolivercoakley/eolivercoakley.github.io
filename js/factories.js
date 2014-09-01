06cf4fdf8ccc7396f715545655c4fe06147dc7a3
Emmett Coakley <eolivercoakley@gmail.com>
2014-09-01 15:09:33 -0700

    Separating the authentication request into two files.

--- js/factories.js
+++ js/factories.js
@@ -7,6 +7,8 @@ myApp.factory('userLoginAuthentication', ['$http',  function($http) {
 	console.error("ATTEMPTING LOGIN INIT!");
 	
   	var authenticationInfo = {};	
+	
+	var authentication_cache;
 		
 	(function (){
 		SE.init({
@@ -29,21 +31,23 @@ myApp.factory('userLoginAuthentication', ['$http',  function($http) {
         // is important; most browsers will hide windows opened without a
         // 'click blessing'
         authenticationInfo.authenticate = function(){
-        	console.error("Attempting to authenticate3");
-	        var a = SE.authenticate({
-	            success: function(data) {
-	                //$scope.loginCredentials = data;
-	                //$scope.$apply();
-	                console.error("Success", data);
-	                return data;
-	            },
-	            error: function(data) {
-	                alert('An error occurred:\n' + data.errorName + '\n' + data.errorMessage);
-	            },
-	            networkUsers: true
-	        });
-	        console.error(a);
-	        return a;    	
+        	return (authentication_cache = SEauthentication(authentication_cache)); 	
+        };
+        
+        function SEauthentication(authentication_cache_object){
+        	if(!authentication_cache_object){
+        		authentication_cache_object = SE.authenticate({
+		            success: function(data) {
+		                console.error("Success", data);
+		                return data;
+		            },
+		            error: function(data) {
+		                alert('An error occurred:\n' + data.errorName + '\n' + data.errorMessage);
+		            },
+		            networkUsers: true
+		        });
+        	}
+        	return authentication_cache_object;
         };
         
     return authenticationInfo;
@@ -55,26 +59,27 @@ myApp.factory('allUserData', ['$http',  function($http) {
 	console.error("Building out the user data request...");
   	
   	var allUserInfo = {};	
+  	var accessToken = "(mZUdl3i(U*5(q*jl*SNkw))";
   	var promise_userInfo, promise_userBadgesInfo, promise_userTimelineInfo, promise_userFavorites, promise_userTagCloudInfo;
   	  	
     allUserInfo.getUserInfo = function(){
-		return (promise_userInfo = generalAPICall(promise_userInfo, "https://api.stackexchange.com/2.2/me?order=desc&sort=reputation&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=ZpEI9T55tXuPT2TjmJRu3A))&callback=JSON_CALLBACK"));
+		return (promise_userInfo = generalAPICall(promise_userInfo, "https://api.stackexchange.com/2.2/me?order=desc&sort=reputation&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=" + accessToken + "&callback=JSON_CALLBACK"));
 	};  
 
     allUserInfo.getUserBadgesInfo = function(){
-    	return (promise_userBadgesInfo = generalAPICall(promise_userBadgesInfo, "https://api.stackexchange.com/2.2/me/badges?order=desc&sort=rank&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=ZpEI9T55tXuPT2TjmJRu3A))&callback=JSON_CALLBACK"));
+    	return (promise_userBadgesInfo = generalAPICall(promise_userBadgesInfo, "https://api.stackexchange.com/2.2/me/badges?order=desc&sort=rank&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=" + accessToken + "&callback=JSON_CALLBACK"));
 	};    
 
     allUserInfo.getUserTimelineInfo = function(){
-    	return (promise_userTimelineInfo = generalAPICall(promise_userTimelineInfo, "https://api.stackexchange.com/2.2/me/timeline?site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=ZpEI9T55tXuPT2TjmJRu3A))&callback=JSON_CALLBACK"));
+    	return (promise_userTimelineInfo = generalAPICall(promise_userTimelineInfo, "https://api.stackexchange.com/2.2/me/timeline?site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=" + accessToken + "&callback=JSON_CALLBACK"));
 	};  
     
 	allUserInfo.getUserFavorites = function(){
-    	return (promise_userFavorites = generalAPICall(promise_userFavorites, "https://api.stackexchange.com/2.2/me/favorites?order=desc&sort=activity&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=ZpEI9T55tXuPT2TjmJRu3A))&callback=JSON_CALLBACK"));
+    	return (promise_userFavorites = generalAPICall(promise_userFavorites, "https://api.stackexchange.com/2.2/me/favorites?order=desc&sort=activity&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=" + accessToken + "&callback=JSON_CALLBACK"));
 	}; 
     
 	allUserInfo.getUserTagCloudInfo = function(){
-    	return (promise_userTagCloudInfo = generalAPICall(promise_userTagCloudInfo, "https://api.stackexchange.com/2.2/me/tags?order=desc&sort=popular&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=ZpEI9T55tXuPT2TjmJRu3A))&callback=JSON_CALLBACK"));
+    	return (promise_userTagCloudInfo = generalAPICall(promise_userTagCloudInfo, "https://api.stackexchange.com/2.2/me/tags?order=desc&sort=popular&site=stackoverflow&key=C8mLfFHVyj1TGEfdDQTEYw((&access_token=" + accessToken + "&callback=JSON_CALLBACK"));
 	};  
 
 	function generalAPICall(promiseData, url){
