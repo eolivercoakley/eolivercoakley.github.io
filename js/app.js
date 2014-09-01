69b9814dca52e8ba3f48dd5a3a655278a526cac1
Emmett Coakley <eolivercoakley@gmail.com>
2014-09-01 16:16:57 -0700

    .

--- js/app.js
+++ js/app.js
@@ -8,15 +8,11 @@ window.globalObject = {
 
 //Singleton to obtain access data from the url before the page is re-routed.
 (function(){
-	console.error("When do we get here?");	
-	console.error("Also current location: ", location.href);
-	console.error("Also current hash: ", location.hash);
 	var urlParams = location.hash.substring(location.hash.indexOf('#') + 1).split('&');
 	for (var i = 0; i < urlParams.length; i++) {
     	var pair = urlParams[i].split('=');
     	window.globalObject[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
   	}
-	console.error(window.globalObject);
 })();
 
 myApp.config(['$routeProvider',
