fb1c3429fb2c45edffdb969aeb53374471d82722
Emmett Coakley <eolivercoakley@gmail.com>
2014-09-01 16:11:34 -0700

    Testing to see if we can grab the access information from the url before the route.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@ var myApp = angular.module('stackExchangeApp', ['ngRoute']);
 
 console.error("Initial location: ", location.href);
 
+window.globalObject = {
+	"access_token" : null
+};
+
+$(document).ready(function(){
+	console.error("When do we get here?");	
+	console.error("Also current location: ", location.href);
+	console.error("Also current hash: ", location.hash);
+	var urlParams = location.hash.substring(location.hash.indexOf('#') + 1).split('&');
+	for (var i = 0; i < urlParams.length; i++) {
+    	var pair = urlParams[i].split('=');
+    	window.globalObject[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+  	}
+	console.error(window.globalObject);
+});
+
 myApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
